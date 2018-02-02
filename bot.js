e3db244284dbfc4564f2d616b622bc65974f46f6
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-02 21:01:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,17 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Ping') {
-    	message.reply('pong');
-  	}
-client.on('message', message => {
     if (message.content === 'Owners') {
     	message.reply('The Owners of this server are @Andresen#3830 and @Bigseth#7672');
   	}
-client.on('message', message => {
-    if (message.content === 'AbortedBot') {
-    	message.reply('This is the official Aborted Discord Bot! Created by @gustings#2804 and @Andresen#3830');
-  	}
+
 });
 
 // THIS  MUST  BE  THIS  WAY

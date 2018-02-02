93e0994d0a0f8affe58c5b135e57561bc0367334
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-02 21:04:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,11 @@ client.on('message', message => {
     if (message.content === 'Owners') {
     	message.reply('The Owners of this server are @Andresen#3830 and @Bigseth#7672');
   	}
-
+client.on('message', message => {
+    if (message.content === 'AbortedBot') {
+    	message.reply('This is the official Aborted Discord Bot! Created by @gustings#2804 and @Andresen#3830');
+  	}
+    
 });
 
 // THIS  MUST  BE  THIS  WAY

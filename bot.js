468fb3f7908098d97028c2e7cc7548870da50c8c
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-02 21:08:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,18 @@ client.on('message', message => {
   	}
     
 });
+    
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'AbortedBot') {
+    	message.reply('This is the official Aborted Discord Bot! Created by @gustings#2804 and @Andresen#3830');
+  	}
+    
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

82fd375814f412e154952363db1825d7ce5d1663
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-05 17:18:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Owners
-client.on('owners', () => {
+// Creator
+client.on('creator', () => {
     console.log('Ready to execute!');
 });
 
 client.on('message', message => {
-    if (message.content === '?owners') {
-    	message.reply('The Owners of this server are Andresen and Bigseth');
-      }
-});
-
-// Creators
-client.on('creators', () => {
-    console.log('Ready to execute!');
-});
-
-client.on('message', message => {
-    if (message.content === '?abortedbot') {
-    	message.reply('This is the official Aborted Discord Bot! Created by gustings and Andresen');
+    if (message.content === '?creator') {
+    	message.reply('This is the official Aborted Discord Bot! Created by Gustings');
   	}
-});
+});    
 
-client.on('info', () => {
-    console.log('Ready to execute!');
-});
 
-client.on('message', message => {
-    if (message.content === '?info') {
-        message.channel.send({embed: {
-           color: 3447003,
-           description: "A very simple Embed!"
-     }
-}});
-        
         
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

474dbf4c1d068efe5676ce8f28a56f648bef31ae
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-05 11:56:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,11 +37,5 @@ client.on('message', message => {
     }
 });
 
-client.on("ready", () => {
-   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-   client.user.setGame(`on ${client.guilds.size} servers`);
-	});
-
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

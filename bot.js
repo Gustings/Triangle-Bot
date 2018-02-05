8c5033dc1dabf2614f9186648b3ccbb7fa167efd
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-05 10:37:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,29 +27,13 @@ client.on('dab', () => {
 
 client.on('message', message => {
     if (message.content === '?help') {
-    	message.channel.send('Test');
-        message.channel.send('Test');
-  	}
-});
-
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '?join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
+    	message.channel.send({embed: {
+  color: 3447003,
+  description: "A very simple Embed!"
     }
-  }
 });
+
+
     
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

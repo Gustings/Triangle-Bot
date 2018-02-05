018388f4109568e3d6d0d5426b88acb571543d72
Gustings <36085056+Gustings@users.noreply.github.com>
2018-02-05 15:31:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,23 +23,5 @@ client.on('message', message => {
   	}
 });
 
-// Info
-/*client.on('info', () => {
-    console.log('Ready to execute!');
-});
-client.on('message', message => {
-    if (message.content === '?info') {
-        message.channel.send('**AbortedBot Info** *Version: 1.0.0*');
-        message.channel.send('**Creator:** Gustings #2804');
-        message.channel.send('**Contributors:**');
-        message.channel.send('Andresen #3830 - ');
-        message.channel.send('Bigseth #7672 - Moral support');
-        message.channel.send('**Website:**');
-        message.channel.send('-SOON-');
-        
-    }
-}); */
-
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

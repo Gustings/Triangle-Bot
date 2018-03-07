5e8b3faacc9d2da7d8b09f74083b90f994b4d692
Gustings <36085056+Gustings@users.noreply.github.com>
2018-03-07 13:53:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Test
-client.on('test', () => {
+// Website
+client.on('creator', () => {
     console.log('Ready to execute!');
 });
 
 client.on('message', message => {
-    if (message.content === '?test') {
-    	message.channel.send('Test Complete');
+    if (message.content === '?website') {
+    	message.channel.send('AbortedBot Website: https://sites.google.com/view/abortedbot');
   	}
-});   
+});    
         
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

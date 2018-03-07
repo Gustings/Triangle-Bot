7465c89ef7596ecaf8ff476c2bac1b4870c4b4bc
Gustings <36085056+Gustings@users.noreply.github.com>
2018-03-07 14:19:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Website
-client.on('test', () => {
+// invite
+client.on('invite', () => {
     console.log('Ready to execute!');
 });
 
 client.on('message', message => {
     if (message.content === ';invite') {
     	message.channel.send('The Aborted Discord: https://discord.gg/qne8NsS');
+  	}
+});    
+
+// rekt
+client.on('rekt', () => {
+    console.log('Ready to execute!');
+});
+
+client.on('message', message => {
+    if (message.content === ';rekt') {
+    	message.channel.send('@156497366130622464');
   	}
 });    
         

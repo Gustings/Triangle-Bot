b2a529d64e99185cdb6a9196a1bef4ef8aeb05c5
Gustings <36085056+Gustings@users.noreply.github.com>
2018-03-07 13:15:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Creator
-client.on('creator', () => {
+// Test
+client.on('test', () => {
     console.log('Ready to execute!');
 });
 
 client.on('message', message => {
-    if (message.content === '?creator') {
-    	message.reply('This is the official Aborted Discord Bot! Created by Gustings');
+    if (message.content === '?test') {
+    	message.channel.send('Test Complete');
   	}
-});    
-
-// Website
-client.on('creator', () => {
-    console.log('Ready to execute!');
-});
-
-client.on('message', message => {
-    if (message.content === '?website') {
-    	message.channel.send('AbortedBot Website: https://sites.google.com/view/abortedbot');
-  	}
-});    
-
+});   
         
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
